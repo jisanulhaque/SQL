a54c4d2af3fee86964f3550101d250c32268a823
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-02 20:17:48 +0600

    Updating/Deleting Data

--- HavingClause.sql
+++ HavingClause.sql
@@ -2,10 +2,18 @@
 Having Clauses
 */
 
-SELECT JobTitle, COUNT(JobTitle)
+--SELECT JobTitle, COUNT(JobTitle)
+--FROM SQLTutorial.dbo.EmployeeDemo
+--JOIN SQLTutorial.dbo.EmployeeSalary 
+--	ON EmployeeDemo.EmployeeID = EmployeeSalary.EmployeeID
+----WHERE COUNT(JobTitle) > 1--Wont work with WHERE Clause
+--GROUP BY JobTitle
+--HAVING COUNT(JobTitle) > 1
+
+SELECT JobTitle, AVG(Salary) AS AverageSalary
 FROM SQLTutorial.dbo.EmployeeDemo
 JOIN SQLTutorial.dbo.EmployeeSalary 
 	ON EmployeeDemo.EmployeeID = EmployeeSalary.EmployeeID
---WHERE COUNT(JobTitle) > 1--Wont work with WHERE Clause
 GROUP BY JobTitle
-HAVING COUNT(JobTitle) > 1
+HAVING AVG(Salary) > 40000 --It will just after GROUP BY
+ORDER BY AVG(Salary)
