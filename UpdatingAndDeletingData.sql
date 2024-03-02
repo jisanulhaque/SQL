10c72293cd2aad921250a0d8646252017cec6788
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-02 20:40:06 +0600

    new scripts added

--- UpdatingAndDeletingData.sql
+++ UpdatingAndDeletingData.sql
@@ -1,3 +1,25 @@
 /*
 Updating and Deleting Data
 */
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemo
+
+INSERT SQLTutorial.dbo.EmployeeDemo VALUES
+(NULL, 'MIKE', 'Tyson', 44, 'Male')
+
+UPDATE SQLTutorial.dbo.EmployeeDemo
+SET EmployeeID = 1011
+WHERE FirstName='MIKE' AND LastName = 'Tyson'
+
+UPDATE SQLTutorial.dbo.EmployeeDemo
+SET FirstName = 'Mike'
+WHERE EmployeeID = 1011
+
+UPDATE SQLTutorial.dbo.EmployeeDemo
+SET Age =33, Gender = 'Female'
+WHERE EmployeeID = 1010
+
+DELETE
+FROM SQLTutorial.dbo.EmployeeDemo
+WHERE EmployeeID = 1011
