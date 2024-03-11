06dc6e4df83b4347199b436c95296a794bb79662
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-11 16:31:21 +0600

    previous topics practiced

---
+++ PartitionBy.sql
@@ -0,0 +1,22 @@
+/*
+Previous Practice
+*/
+--Aliasing 
+--SELECT*
+--FROM SQLTutorial.dbo.EmployeeDemo demo
+--JOIN SQLTutorial.dbo.EmployeeSalary sal
+--ON demo.EmployeeID = sal.EmployeeID
+
+--SELECT FirstName + ' '+ LastName FullName
+--FROM SQLTutorial.dbo.EmployeeDemo demo
+
+--SELECT*
+--FROM SQLTutorial.dbo.EmployeeSalary
+
+--UPDATE SQLTutorial.dbo.EmployeeSalary
+--SET EmployeeID = 1010 WHERE JobTitle = 'Watchman'
+
+/*
+Topic: PARTITION BY
+*/
+
