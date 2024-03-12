9271bc302253ca559fac7534f3ceb788530c31fb
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-12 10:04:15 +0600

    practice added

--- CommonTableExpression.sql
+++ CommonTableExpression.sql
@@ -2,13 +2,24 @@
 TOPIC: Common Table Expression(CTE)
 */
 
+--WITH CTE_Employee as(SELECT FirstName, LastName, Gender, Salary
+--,COUNT(Gender) OVER(PARTITION BY Gender)as TotalGender
+--,Avg(Salary) OVER(PARTITION BY Gender) as AvgSal
+--FROM SQLTutorial..EmployeeDemo as emp
+--JOIN SQLTutorial..EmployeeSalary as sal
+--	ON emp.EmployeeID = sal.EmployeeID
+--WHERE Salary > '45000'
+--)
+--SELECT *
+--FROM CTE_Employee
+
 WITH CTE_Employee as(SELECT FirstName, LastName, Gender, Salary
 ,COUNT(Gender) OVER(PARTITION BY Gender)as TotalGender
-,Avg(Salary) OVER(PARTITION BY Gender) as AvgSal
+,Avg(Salary) OVER(PARTITION BY FirstName)as AvgSal
 FROM SQLTutorial..EmployeeDemo as emp
 JOIN SQLTutorial..EmployeeSalary as sal
 	ON emp.EmployeeID = sal.EmployeeID
-WHERE Salary > '45000'
+--WHERE Salary > '45000'
 )
 SELECT *
 FROM CTE_Employee
