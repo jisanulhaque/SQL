33d447e9127634183c424eca35bc3d3efbb851d4
JisanulHaque <jisanulhaque1996@gmail.com>
2024-06-05 18:38:08 +0600

    Auto Increment added

---
+++ AutoIncrement.sql
@@ -0,0 +1,11 @@
+create table Offrs(
+SLno int not null Identity(10,5) Primary key,
+FullName varchar(255) not null,
+Branch varchar(255) not null,
+);
+
+select * from Offrs
+
+insert into Offrs values--(FullName, Branch)
+('Munim','ADWC')
+
